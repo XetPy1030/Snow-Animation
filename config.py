d706975540d47bdc04fc03e993621de96b2b5ada
XetPy1030 <xetpy1030@mail.ru>
2024-10-02 15:39:50 +0300

    Migrate game to sprite class

--- config.py
+++ config.py
@@ -1,13 +1,21 @@
 from random import randint
 
-BACKGROUND = (30, 30, 30)
-
+# Size of the snowflakes
 MIN_SIZE = 3
 MAX_SIZE = 6
 
+# Color of the snowflakes
 COLOR_SNOWFLAKE = lambda: randint(200, 255)
 
+# Speed of the snowflakes
 MIN_SPEED = 7
 MAX_SPEED = 11
 
+# Wind speed, offset for the snowflakes
+OFFSET_X = -300
 WIND_SPEED = [1.5, 1]
+
+FPS = 60
+
+# Chance of a new snowflake
+CHANCE_NEW_SNOWFLAKE = 50
--- main.py
+++ main.py
@@ -1,69 +1,93 @@
-from dataclasses import dataclass
-from random import randint
+import random
 
 import pygame
 
 import config
 
-pygame.init()
 
-screen = pygame.display.set_mode([0, 0], pygame.FULLSCREEN)
+class Snowflake(pygame.sprite.Sprite):
+    """
+    This class represents the snowflake.
 
-width = screen.get_width()
-height = screen.get_height()
+    It derives from the "Sprite" class in Pygame.
+    """
 
-background = pygame.image.load('background.jpg')
-background = pygame.transform.scale(background, (width, height))
+    containers = None
+
+    def __init__(self, x, y, size, color, height):
+        super().__init__(self.containers)
+
+        # Create the snowflake
+        self.image = pygame.Surface([size * 2, size * 2])
+        self.image.fill((0, 0, 0))
+        self.image.set_colorkey((0, 0, 0))
+        pygame.draw.circle(self.image, color, (size, size), size)
+
+        # Set the position of the snowflake
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+        # Set the speed of the snowflake based on its size
+        self.speed_y = (size - config.MIN_SIZE) / (config.MAX_SIZE - config.MIN_SIZE) + 1
+        # Set the height of the screen for the snowflake to disappear
+        self.height = height
+
+    def update(self):
+        self.rect.y += self.speed_y + config.WIND_SPEED[0]
+        self.rect.x += config.WIND_SPEED[1]
+        if self.rect.y > self.height:
+            self.kill()
 
 
-@dataclass
-class Snowflake:
-    x: int
-    y: float
-    size: int
-    speed_y: int
-    speed_x: int
-    color: list[int]
+def main():
+    # Initialize Pygame
+    pygame.init()
+
+    # Set up the screen
+    screen = pygame.display.set_mode([0, 0], pygame.FULLSCREEN)
+
+    # Get screen dimensions
+    width = screen.get_width()
+    height = screen.get_height()
+
+    # Set up the background
+    background = pygame.image.load('background.jpg')
+    background = pygame.transform.scale(background, (width, height))
+
+    # Initialize Groups
+    snowflakes = pygame.sprite.Group()
+    all_sprites = pygame.sprite.Group()
+
+    # Add the sprite to the groups
+    Snowflake.containers = snowflakes, all_sprites
+
+    # Create clock
+    clock = pygame.Clock()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.blit(background, (0, 0))
+
+        all_sprites.update()
+
+        # Generate new snowflakes
+        if random.randint(0, 100) < config.CHANCE_NEW_SNOWFLAKE:
+            size = random.randint(config.MIN_SIZE, config.MAX_SIZE)
+            color = [config.COLOR_SNOWFLAKE() for _ in range(3)]
+            x = random.randint(config.OFFSET_X, width)
+            Snowflake(x, -30, size, color, height)
+
+        all_sprites.draw(screen)
+
+        pygame.display.flip()
+        clock.tick(config.FPS)
 
 
-snowflakes = []
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    screen.blit(background, (0, 0))
-
-    if randint(0, 1) == 0:
-        size = randint(config.MIN_SIZE, config.MAX_SIZE)
-        color = [config.COLOR_SNOWFLAKE() for _ in range(3)]
-        speed_y = (size - config.MIN_SIZE) / (config.MAX_SIZE - config.MIN_SIZE) + 1
-
-        snowflakes.append(
-            Snowflake(
-                randint(-300, width),
-                -30,
-                size,
-                speed_y,
-                0,
-                color
-            )
-        )
-    for snowflake in snowflakes:
-        pygame.draw.circle(
-            screen,
-            snowflake.color,
-            (snowflake.x, int(snowflake.y)),
-            snowflake.size
-        )
-        snowflake.y += snowflake.speed_y + config.WIND_SPEED[0]
-        snowflake.x += snowflake.speed_x + config.WIND_SPEED[1]
-
-        if snowflake.y > height:
-            del snowflake
-
-    pygame.display.flip()
-
-pygame.quit()
+if __name__ == '__main__':
+    main()
+    pygame.quit()
